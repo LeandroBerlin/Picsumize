03a1a1e2a3d246d92f45b1ecf50017b748950421
Leandro <leandro.frigerio@devugees.org>
2019-12-16 15:37:43 +0100

    Title component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/App.css';
 import Picsum from './Picsum';
+import Title from './Title'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Title text="Picsum API" />
         <Picsum />
       </header>
     </div>
