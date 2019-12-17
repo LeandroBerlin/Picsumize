4d87ba54f2e810df03f8f6d833ef15832091c270
Leandro <leandro.frigerio@devugees.org>
2019-12-17 15:15:41 +0100

    Updates

---
+++ precache-manifest.55f9d340e0b68e4a19f3aacdfb54e5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbfea1a2752bf80ab0a12e7c6ce3362",
+    "url": "/Picsumize/index.html"
+  },
+  {
+    "revision": "6060eb7ec23fa37a3c91",
+    "url": "/Picsumize/static/css/main.7cc3caa5.chunk.css"
+  },
+  {
+    "revision": "b2cd8fa03f7d5b80ad1c",
+    "url": "/Picsumize/static/js/2.843fec27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Picsumize/static/js/2.843fec27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6060eb7ec23fa37a3c91",
+    "url": "/Picsumize/static/js/main.5a33e02d.chunk.js"
+  },
+  {
+    "revision": "ff91dbbf11c7efcc53ad",
+    "url": "/Picsumize/static/js/runtime-main.39013748.js"
+  }
+]);
